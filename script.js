d33ca220c95ae27eff164a4c91772328a8a6d53c
Shawna Cooper <scooper1920@gmail.com>
2022-04-02 17:39:47 -0400

    (stick a fork in this, it's done)

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 console.log("Wassup")
+
+
+function play() {
+    var audio = new Audio(".github/starter_files/ANMLCat_Little meow of a cat 7 (ID 1477)_BSB.mp3");
+    audio.play()
+    console.log("played dat kitty")
+}
+
+const numButtons = document.querySelectorAll(".button");
+console.log("All the buttons in the land", numButtons);
+
+const display = document.getElementById("display");
+console.log("The grand div of display", display)
+
+for (let button of numButtons) {
+    button.addEventListener("click",(Event) => {
+
+        if(Event.target.id !== "eval") {
+            display.innerText += Event.target.id;
+            console.log("The event object is", Event.target.id);
+        }
+        if (Event.target.id === "clear") {
+            display.innerText = ""
+            console.log("All cleared up!")
+        }
+
+        if (Event.target.id === "eval") {
+            let result = eval(display.innerText)
+            display.innerText = result
+            console.log("result")
+            console.log("Eval Clicked! Oh Yeeaaah Baybay!")
+        }
+    });
+    
+}
